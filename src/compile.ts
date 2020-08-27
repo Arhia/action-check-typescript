341f0e8d0f8b271383b78e9694f6ffb2e0531164
Arnaud Benhamdine <abenhamdine@users.noreply.github.com>
2020-08-28 00:48:30 +0200

    Formatting

--- src/compile.ts
+++ src/compile.ts
@@ -1,28 +1,16 @@
 import * as ts from 'typescript'
 
-export function compile(
-  fileNames: string[],
-  options: ts.CompilerOptions
-): void {
+export function compile(fileNames: string[], options: ts.CompilerOptions): void {
   const program = ts.createProgram(fileNames, options)
   const emitResult = program.emit()
 
-  const allDiagnostics = ts
-    .getPreEmitDiagnostics(program)
-    .concat(emitResult.diagnostics)
+  const allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics)
 
   allDiagnostics.forEach(diagnostic => {
     if (diagnostic.file) {
-      let {line, character} = diagnostic.file.getLineAndCharacterOfPosition(
-        diagnostic.start!
-      )
-      let message = ts.flattenDiagnosticMessageText(
-        diagnostic.messageText,
-        '\n'
-      )
-      console.log(
-        `${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`
-      )
+      let { line, character } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start!)
+      let message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n')
+      console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`)
     } else {
       console.log(ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n'))
     }
