a03f549b66112c200cac1bcc19241447e8b278b9
Arnaud Benhamdine <abenhamdine@users.noreply.github.com>
2020-08-27 23:35:04 +0200

    Add compile fonction

---
+++ src/compile.ts
@@ -0,0 +1,34 @@
+import * as ts from 'typescript'
+
+export function compile(
+  fileNames: string[],
+  options: ts.CompilerOptions
+): void {
+  const program = ts.createProgram(fileNames, options)
+  const emitResult = program.emit()
+
+  const allDiagnostics = ts
+    .getPreEmitDiagnostics(program)
+    .concat(emitResult.diagnostics)
+
+  allDiagnostics.forEach(diagnostic => {
+    if (diagnostic.file) {
+      let {line, character} = diagnostic.file.getLineAndCharacterOfPosition(
+        diagnostic.start!
+      )
+      let message = ts.flattenDiagnosticMessageText(
+        diagnostic.messageText,
+        '\n'
+      )
+      console.log(
+        `${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`
+      )
+    } else {
+      console.log(ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n'))
+    }
+  })
+
+  const exitCode = emitResult.emitSkipped ? 1 : 0
+  console.log(`Process exiting with code '${exitCode}'.`)
+  process.exit(exitCode)
+}
