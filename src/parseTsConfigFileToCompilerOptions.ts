011b5cb3b5d18558f3e09635cc96951e5bf3b01a
Arnaud Benhamdine <abenhamdine@users.noreply.github.com>
2020-08-28 14:07:18 +0200

    Try/catch parsing

--- src/parseTsConfigFileToCompilerOptions.ts
+++ src/parseTsConfigFileToCompilerOptions.ts
@@ -4,7 +4,13 @@ import * as fs from 'fs'
 
 export function parseTsConfigFileToCompilerOptions(configPath: string): ts.CompilerOptions {
     const content = fs.readFileSync(configPath).toString()
-    const parsed = ts.parseJsonConfigFileContent(JSON.parse(content), ts.sys, path.dirname(configPath))
-    const compilerOptions = parsed.options
+    let parsed: ts.ParsedCommandLine
+    let compilerOptions: ts.CompilerOptions
+    try {
+        parsed = ts.parseJsonConfigFileContent(JSON.parse(content), ts.sys, path.dirname(configPath))
+        compilerOptions = parsed.options
+    } catch (error) {
+        throw new Error(`Error while parsing tsconfig and converting to compiler options \n ${error.message}`)
+    }
     return compilerOptions
 }
